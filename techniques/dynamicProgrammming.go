fdc2c4b771dec5956968300be36a766ff7636eac
Tunde <t_afolabi@yahoo.com>
2022-09-13 19:09:41 -0400

    add buggy 2DKnapsackproblem

--- techniques/dynamicProgrammming.go
+++ techniques/dynamicProgrammming.go
@@ -54,3 +54,102 @@ func TabulationFibonacci(n int) int {
 	}
 	return fibArray[n]
 }
+
+func BruteForceKnapsack(weights, profit []int, capacity int) int {
+	//store maximum capacity
+	//loop through weights
+	//for each weight
+	//check if current weight picked is higher than maxCap
+	//if it is update max cap
+	// then continue to add to each other weight
+	// if weight is less than or equal to cap
+	// add profits together
+	// compare addition to existing max cap
+	// update added figure
+	//return max cap
+
+	var maxCap int
+	// abcd
+	// 1,2,3,5
+	// 1,6,10,16
+	// c-> 7
+	for weightKey, weightValue := range weights {
+		if weightValue <= capacity {
+			if profit[weightKey] > maxCap {
+				maxCap = profit[weightKey]
+			}
+
+			for i := 0; i < len(weights); i++ {
+				if i != weightKey {
+					currentWeight := weights[weightKey] + weights[i]
+					if currentWeight <= capacity {
+						currentProfit := profit[weightKey] + profit[i]
+						if currentProfit > maxCap {
+							maxCap = currentProfit
+						}
+					}
+				}
+			}
+		}
+	}
+	return maxCap
+}
+
+func RecursionKnapsack(weights, profits []int, capacity int) int {
+	// abcd
+	// 1,2,3,5
+	// 1,6,10,16
+	// c-> 7
+	return recursionKnapsack(weights, profits, capacity, 0)
+}
+
+func recursionKnapsack(weights []int, profits []int, capacity, currentIndex int) int {
+	//base checks
+	if capacity <= 0 || currentIndex >= len(profits) {
+		return 0
+	}
+	profitOne := 0
+	// recursive call after choosing the element at the currentIndex
+	// if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
+	if weights[currentIndex] <= capacity {
+		profitOne = recursionKnapsack(weights, profits, capacity-weights[currentIndex], currentIndex+1)
+	}
+
+	// recursive call after excluding the element at the currentIndex
+	profitTwo := recursionKnapsack(weights, profits, capacity, currentIndex)
+
+	return max(profitOne, profitTwo)
+
+}
+
+func max(numberOne, numberTwo int) int {
+	if numberOne > numberTwo {
+		return numberOne
+	}
+	return numberTwo
+}
+
+func Knapsack2dProblem(items [][]int, capacity int) []interface{} {
+	return recursionSolution(items, capacity, 0, 0, []int{}, len(items))
+}
+
+func recursionSolution(items [][]int, capacity, i, currentValue int, indices []int, n int) []interface{} {
+	if capacity <= 0 || n <= 0 {
+		return []interface{}{currentValue, []int{}}
+	}
+
+	value := items[i][0]
+	weight := items[i][1]
+
+	exclude := []interface{}{0, []int{}}
+	if capacity > weight {
+		exclude = recursionSolution(items, capacity, i-1, currentValue, indices, n-1)
+	}
+
+	include := recursionSolution(items, capacity-weight, i-1, currentValue+value, indices, n-1)
+
+	if include[0].(int) >= exclude[0].(int) {
+		return []interface{}{include[0], include[1]}
+	}
+	return []interface{}{exclude[0], exclude[1]}
+}
--- techniques/dynamicProgrammming_test.go
+++ techniques/dynamicProgrammming_test.go
@@ -38,3 +38,64 @@ func TestFactorial(t *testing.T) {
 		})
 	}
 }
+
+func TestKnapSack(t *testing.T) {
+	type args struct {
+		weights, profits []int
+		capacity         int
+	}
+
+	tests := []struct {
+		name   string
+		inputs args
+		want   int
+	}{
+		{
+			name: "brute force test",
+			inputs: args{
+				weights:  []int{1, 2, 3, 5},
+				profits:  []int{1, 6, 10, 16},
+				capacity: 7,
+			},
+			want: 22,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if received := RecursionKnapsack(test.inputs.weights, test.inputs.profits, test.inputs.capacity); received != test.want {
+				t.Errorf("failed test")
+			}
+		})
+	}
+}
+
+func TestKnapsack2dProblem(t *testing.T) {
+	type args struct {
+		weightsValue [][]int
+		capacity     int
+	}
+
+	tests := []struct {
+		name  string
+		input args
+		want  []interface{}
+	}{
+		{
+			name: "valid numbers",
+			input: args{
+				[][]int{[]int{1, 2}, []int{4, 3}, []int{5, 6}, []int{6, 7}},
+				10,
+			},
+			want: []interface{}{10, []int{1, 3}},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if received := Knapsack2dProblem(test.input.weightsValue, test.input.capacity); received[1] != test.want[1] {
+				t.Errorf("failed test ")
+			}
+		})
+	}
+}
